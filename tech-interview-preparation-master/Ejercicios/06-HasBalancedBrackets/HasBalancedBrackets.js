24a5b960ea4426468c392b48e10498a4528d1c69
Juan Esteban <juancho50301@gmail.com>
2023-06-07 18:29:14 -0500

    'ejercicio-06'

--- tech-interview-preparation-master/Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ tech-interview-preparation-master/Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,43 @@
-function HasBalancedBrackets (string) {
-  // Your code here:
+function hasBalancedBrackets(string) {
+  const stack = [];
 
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    if (isOpenBracket(char)) {
+      stack.push(char);
+    } else if (isCloseBracket(char)) {
+      if (stack.length === 0 || !bracketsMatch(stack.pop(), char)) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
+function isOpenBracket(char) {
+  return char === '[' || char === '(' || char === '{';
+}
+
+function isCloseBracket(char) {
+  return char === ']' || char === ')' || char === '}';
+}
+
+function bracketsMatch(openBracket, closeBracket) {
+  return (
+    (openBracket === '[' && closeBracket === ']') ||
+    (openBracket === '(' && closeBracket === ')') ||
+    (openBracket === '{' && closeBracket === '}')
+  );
+}
+
+// Ejemplos de uso:
+console.log(hasBalancedBrackets('{[]()}'));  // Salida: true
+console.log(hasBalancedBrackets("{[(])}"));  // Salida: false
+console.log(hasBalancedBrackets("{[("));    // Salida: false
+console.log(hasBalancedBrackets("{[([{()[]{}}])]}"));        // Salida: false
+console.log(hasBalancedBrackets("{[]}}"));  // Salida: true
+
 
 module.exports = HasBalancedBrackets
